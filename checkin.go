529b3f55f2ed8459d7b3ee80be229c66c4e4c88e
Hywfred <hitwhy@gmail.com>
2022-10-17 09:18:21 +0000

    add a function to generate a 16 bit long random string

--- checkin.go
+++ checkin.go
@@ -3,8 +3,12 @@ package goiqiyi
 import (
 	"encoding/json"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
+	"unicode/utf16"
 )
 
 // Query gets current status of the user specified by p00001
@@ -34,4 +38,24 @@ func Query(p00001 string, target interface{}) {
 		log.Println(err)
 		os.Exit(1)
 	}
+}
+
+// GenerateRandomStr spawns a random string
+func GenerateRandomStr(len int) string {
+	rand.Seed(time.Now().UnixNano())
+	str := ""
+	for i := 0; i < len; i++ {
+		rdm62 := uint16(math.Floor(rand.Float64() * 62))
+		var code uint16 = 0
+		if rdm62 < 10 {
+			code = rdm62 + 48
+		} else if rdm62 < 36 {
+			code = rdm62 + 55
+		} else {
+			code = rdm62 + 61
+		}
+		coderune := utf16.Decode([]uint16{code})[0]
+		str += string(coderune)
+	}
+	return str
 }
